7765cb45d1ec59cf1d7b39deb7c185a568c45524
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-07-30 00:14:49 +0300

    Start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const onCreateGalery = (large, small, alt) => {
+  return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${large}">
+        <img
+            class="gallery__image"
+            src="${small}"
+            data-source="${large}"
+            alt="${alt}"
+        />
+    </a>
+</div>`;
+};
+
+const calcGalery = galleryItems
+  .map((item) => {
+    return onCreateGalery(item.original, item.preview, item.description);
+  })
+  .join("");
+
+galery.insertAdjacentHTML("afterbegin", calcGalery);
+
+function hadleClick(e) {
+  e.preventDefault();
+
+  if (e.currentTarget.nodeName !== e.target.nodeName) {
+    const imgSource = e.target.dataset;
+    const { source } = imgSource;
+
+    createModalLightBox(source);
+  }
+}
+
+function createModalLightBox(source) {
+  const instance = basicLightbox.create(`<img src="${source}">`);
+  instance.show();
+
+  const visible = instance.visible();
+
+  if (visible) {
+    window.addEventListener(
+      "keydown",
+      (evt) => {
+        const { code } = evt;
+        if (evt.target.nodeName === "A" && code === "Escape") {
+          instance.close();
+        }
+      },
+      { once: true }
+    );
+  }
+}
+
+// listener
+galery.addEventListener("click", hadleClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const onCreateGalery = (large, small, alt) => {
+  return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${large}">
+        <img
+            class="gallery__image"
+            src="${small}"
+            alt="${alt}"
+        />
+    </a>
+</div>`;
+};
+
+const calcGalery = galleryItems
+  .map((item) => {
+    return onCreateGalery(item.original, item.preview, item.description);
+  })
+  .join("");
+
+galery.insertAdjacentHTML("afterbegin", calcGalery);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox");
